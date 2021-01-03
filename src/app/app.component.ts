5a8a2c1918588d29564ee00d15f9937d1ccc80c1
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2021-01-03 14:19:32 +0100

    add start screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  stars() {
+    var COUNT_STAR = 40;
+    function rand(limit: number) {
+      return Math.random() * limit + 1;
+    }
+    var stars = document.getElementsByClassName('space')[0];
+    for (var i = 0; i < COUNT_STAR; i++) {
+      var star = document.createElement('div');
+      var style = `left: ${rand(98)}%; top: ${rand(98)}%;`;
+      star.className = 'star';
+      star.setAttribute('style', style);
+      stars.appendChild(star);
+    }
+  }
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { StartComponent } from './start/start.component';
+
+@NgModule({
+  declarations: [StartComponent],
+  exports: [StartComponent],
+})
+export class CoreModule {}
